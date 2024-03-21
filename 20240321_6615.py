603430e15eeabd85a285719e2f7dbeeafa9f77f2
tjkpolisher <jktak5609@gmail.com>
2024-03-21 23:46:29 +0900

    주석 단어 수정(시퀀스 → 수열)

--- 20240321_6615.py
+++ 20240321_6615.py
@@ -16,7 +16,7 @@ def Collatz(n):
 
 
 def find_meeting_point(A, B):
-    # A와 B의 콜라츠 시퀀스에서 같은 수가 나타나는 첫 번째 지점을 찾는 함수
+    # A와 B의 콜라츠 수열에서 같은 수가 나타나는 첫 번째 지점을 찾는 함수
     steps_A, steps_B = {A: 0}, {B: 0}
     seq_A, seq_B = A, B
     step_count_A, step_count_B = 0, 0
@@ -26,7 +26,7 @@ def find_meeting_point(A, B):
             seq_A = Collatz(seq_A)
             step_count_A += 1
             if seq_A in steps_B:
-                # A의 시퀀스가 B의 시퀀스에 이미 나타난 수를 만났을 때
+                # A의 수열이 B의 수열에 이미 나타난 수를 만났을 때
                 return A, step_count_A, B, steps_B[seq_A], seq_A
             steps_A[seq_A] = step_count_A
 
@@ -34,7 +34,7 @@ def find_meeting_point(A, B):
             seq_B = Collatz(seq_B)
             step_count_B += 1
             if seq_B in steps_A:
-                # B의 시퀀스가 A의 시퀀스에 이미 나타난 수를 만났을 때
+                # B의 수열이 A의 수열에 이미 나타난 수를 만났을 때
                 return A, steps_A[seq_B], B, step_count_B, seq_B
             steps_B[seq_B] = step_count_B
 
